5c467a872d86aad581e5108dd3d0821ab4123ce7
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-22 14:47:30 +0100

    Add initial Package.swift

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-riscv-primitives",
+    platforms: [
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26),
+        .visionOS(.v26),
+    ],
+    products: [
+        .library(
+            name: "RISCV Primitives",
+            targets: ["RISCV Primitives"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../swift-cpu-primitives"),
+    ],
+    targets: [
+        .target(
+            name: "RISCV Primitives",
+            dependencies: [
+                .product(name: "CPU Primitives", package: "swift-cpu-primitives"),
+            ]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
+
+for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
+    let settings: [SwiftSetting] = [
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("MemberImportVisibility"),
+        .enableExperimentalFeature("Lifetimes"),
+        .strictMemorySafety(),
+    ]
+    target.swiftSettings = (target.swiftSettings ?? []) + settings
+}
